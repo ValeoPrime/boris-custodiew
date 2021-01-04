9dd672c7fe663f713b6b5ceb698235194ba61a22
Valentin <primarh111@mail.ru>
2021-01-04 08:10:39 +0300

    initial commit (add fonts and mock data)

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,25 @@
   <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+<style lang="sass">
+#app
+  font-family: YesevaOne-Regular, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
 
-#nav {
+
+#nav
   padding: 30px;
 
-  a {
+  a
     font-weight: bold;
     color: #2c3e50;
 
-    &.router-link-exact-active {
+    &.router-link-exact-active
       color: #42b983;
-    }
-  }
-}
+
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './sass/main.sass'
+
 
 createApp(App).use(router).mount('#app')
